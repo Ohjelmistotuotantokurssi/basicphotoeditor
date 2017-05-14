a0646a5a3d4b0ab4fad767310abaa7a07c1faca9
Samuli Kumo <samuli.kumo@gmail.com>
2017-05-14 15:22:59 +0300

    Initial commit, basic program structure

---
+++ basicphotoeditor/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace basicphotoeditor
+{   
+    public partial class Form1 : Form
+    {
+        private string TAG;
+        public Form1()
+        {
+            InitializeComponent();
+            TAG = GetType().Name;
+        }
+
+        private void onClick(object sender, EventArgs e)
+        {
+            //Do something when button is clicked
+            Console.WriteLine(TAG + ": onClick()");
+        }
+    }
+}
