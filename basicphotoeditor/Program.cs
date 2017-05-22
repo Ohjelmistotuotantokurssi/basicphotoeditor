052d92ada86e45800b96ba6b33f9f40375f98534
Samuli Kumo <samuli.kumo@gmail.com>
2017-05-22 20:04:53 +0300

    testing

--- basicphotoeditor/Program.cs
+++ basicphotoeditor/Program.cs
@@ -9,7 +9,7 @@ namespace basicphotoeditor
     static class Program
     {
         /// <summary>
-        /// The main entry point for the application.
+        /// The main entry point for the application. 
         /// </summary>
         [STAThread]
         static void Main()
